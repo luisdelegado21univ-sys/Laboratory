680d15dad222f5faa84808a11f26e75cf0e55c27
luisdelegado21univ <luisdelegado21univ@gmail.com>
2026-09-24 18:54:04 -0500

    Ingrese una calculadora

---
+++ Laboratory001N2LD.py
@@ -0,0 +1,7 @@
+print ("Una calculadora de suma +. resta -, multiplicación * y division /")
+number1= 60
+number2= 20
+print("El resultado de la suma es: ", number1 + number2)
+print("El resultado de la resta es ", number1 - number2)
+print("El  resultado de la multiplicación es ",number1 * number2)
+print("El resultado de la división es ",number1 / number2)

e0c2d92e4d391b07aba9beb8dbe43eaadcd7094b
luisdelegado21univ <luisdelegado21univ@gmail.com>
2026-09-24 18:58:10 -0500

    Ingrese mi nombre en colores

---
+++ Laboratory001N3LD.py
@@ -0,0 +1,9 @@
+print ("Mi nombre en colores")
+RED = '\033[31m'
+BLUE = '\033[34m'
+YELOW = '\033[33m'
+GREEN = '\033[32m'
+
+print(f" {RED}Como te llamas?")
+print(f" {BLUE}Mi nombre es:{GREEN} LUIS DELEGADO")
+print(f" {YELOW}Mucho gusto en conocerte")
